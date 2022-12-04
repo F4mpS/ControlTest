ce5a0971500e93d02f0f76424fb0e90d57d73c40
F4mpS <sany2k13@mail.ru>
2022-12-04 17:40:36 +0300

    Добавлены пояснения к коду

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,4 +1,4 @@
-﻿int CountStrings(string[] list)
+﻿int CountStrings(string[] list)     // Получение количества строк с длинной меньшей или равной 3
 {
     int lenght = list.Length;
     int count = 0;
@@ -12,10 +12,10 @@
 }
 
 
-string[] SortList(string[] list)
+string[] SortList(string[] list)    // Сортировка заданного массива по условию задачи
 {
     int sortedListLenght = CountStrings(list);
-    int startPos = 0;
+    int startPos = 0;   // 0 элемент отсортированного массива
     string[] sortedList = new string[sortedListLenght];
 
     for (int i = 0; i < list.Length; i++)
@@ -23,7 +23,7 @@ string[] SortList(string[] list)
         if (list[i].Length <= 3)
         {
             sortedList[startPos] = list[i];
-            startPos++;
+            startPos++;     // Переход на следующий элемент отсортированного массива
         }
     }
 
@@ -31,13 +31,13 @@ string[] SortList(string[] list)
 }
 
 
-string GetList(string[] list)
+string GetList(string[] list)   // Вывод любого массива
 {
     return "[" + string.Join(", " , list) + "]";
 }
 
 
-Console.Write("Введите размер массива строк >>> ");
+Console.Write("Введите длинну массива строк >>> ");
 int l = Convert.ToInt32(Console.ReadLine());
 string[] userList = new string[l];
 string userString;
